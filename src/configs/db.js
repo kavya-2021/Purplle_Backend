d73ebe1fc9ae9a49d45d612644bd385316622c0a
Lakshmi Kavya <vankadaralakshmikavya@gmail.com>
2022-01-21 10:43:11 +0530

    Confirm Delete

--- src/configs/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = ()=>{
-    return mongoose.connect("mongodb+srv://kavya_047:kavya_047@cluster0.vrril.mongodb.net/Purplle_Database")
-}
--- src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./index');
-
-
-const connect = require('./configs/db')
-
-app.listen("4905",async()=>{
-    await connect();
-    console.log("Listening to port 4905");
-})
