0ccf35d1ca085a725246b423db603039ced31d9d
Mayuri <mayuriwasu2000@gmail.com>
2022-01-21 23:40:36 +0530

    added working auth

--- src/index.js
+++ src/index.js
@@ -1,16 +1,64 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
 app.use(express.json());
 
-app.set('view engine', 'ejs');
 
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
 const Homepage = require('./controllers/home.controller')
-
-
 app.use("/" , Homepage)
+
+
+// Passport Config
+require('./configs/passport')(passport);
+
+// DB Config
+const db = require('./configs/keys').mongoURI;
+
+// Express session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
+  
+  // Routes
+  app.use('/', require('./controllers/auth.controller.js'));
+  app.use('/users', require('./controllers/users.controller.js'));
+  
+
+
+
 // crud for the ejs control of facemakeup products
 
 const faceController = require('./controllers/face.controller');
@@ -27,7 +75,7 @@ app.use("/hairproducts",hairController);
 
 const skinController = require('./controllers/skin.controller');
 
-app.use("/skinproducts",skinController);
+app.use("/skinproducts",skinController);  
 
 // crud for the ejs control of Wish List products
 
