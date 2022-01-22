fba76427a813401c914cdd2c2960f2335c70b833
Lakshmi Kavya <vankadaralakshmikavya@gmail.com>
2022-01-22 17:41:21 +0530

    update2

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = require('./index');
 
 const connect = require('./configs/db')
 
-app.listen( process.env.MONGODB_URI || "4905",async()=>{
+app.listen( process.env.PORT || 4905 ,async()=>{
     await connect();
     console.log("Listening to port 4905");
 })
